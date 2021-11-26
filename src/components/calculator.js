80e971d1aa923141bf9f360d99001d76add9f96c
blessedjasonmwanza <mwanzabj@gmail.com>
2021-11-26 13:18:34 +0200

    Use hooks in calculator .js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,71 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import calculate from '../logic/calculate';
 import Answer from './Answer';
 import './css/calculator.css';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [state, callBack] = useState({
+    total: '0',
+    next: null,
+    operation: null,
+  });
 
-  componentDidMount() {
+  useEffect(() => {
     const btns = document.querySelectorAll('button');
     Array.from(btns).forEach((btn) => {
       btn.addEventListener('click', () => {
         const value = btn.getAttribute('data');
-        this.setState((currState) => calculate(currState, value));
+        callBack(calculate(state, value));
       });
     });
-  }
+  }, [state]);
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator cols">
-        <Answer
-          total={total}
-          next={next}
-          operation={operation}
-        />
-        <span className="d-flex row">
-          <button type="button" data="AC">AC</button>
-          <button type="button" data="+/-">
-            +
-            <small>/</small>
-            -
-          </button>
-          <button type="button" data="%">%</button>
-          <button type="button" data="/" className="operator">&divide;</button>
-        </span>
-        <span className="d-flex row">
-          <button type="button" data="7">7</button>
-          <button type="button" data="8">8</button>
-          <button type="button" data="9">9</button>
-          <button type="button" data="x" className="operator">&times;</button>
-        </span>
-        <span className="d-flex row">
-          <button type="button" data="4">4</button>
-          <button type="button" data="5">5</button>
-          <button type="button" data="6">6</button>
-          <button type="button" data="-" className="operator">-</button>
-        </span>
-        <span className="d-flex row">
-          <button type="button" data="1">1</button>
-          <button type="button" data="2">2</button>
-          <button type="button" data="3">3</button>
-          <button type="button" data="+" className="operator">+</button>
-        </span>
-        <span className="d-flex row">
-          <button type="button" data="0" className="zero">0</button>
-          <button type="button" data=".">.</button>
-          <button type="button" data="=" className="operator">=</button>
-        </span>
-      </div>
-    );
-  }
-}
+  const { total, next, operation } = state;
+  return (
+    <div className="calculator cols">
+      <Answer
+        total={total}
+        next={next}
+        operation={operation}
+      />
+      <span className="d-flex row">
+        <button type="button" data="AC">AC</button>
+        <button type="button" data="+/-">
+          +
+          <small>/</small>
+          -
+        </button>
+        <button type="button" data="%">%</button>
+        <button type="button" data="÷" className="operator">&divide;</button>
+      </span>
+      <span className="d-flex row">
+        <button type="button" data="7">7</button>
+        <button type="button" data="8">8</button>
+        <button type="button" data="9">9</button>
+        <button type="button" data="x" className="operator">&times;</button>
+      </span>
+      <span className="d-flex row">
+        <button type="button" data="4">4</button>
+        <button type="button" data="5">5</button>
+        <button type="button" data="6">6</button>
+        <button type="button" data="-" className="operator">-</button>
+      </span>
+      <span className="d-flex row">
+        <button type="button" data="1">1</button>
+        <button type="button" data="2">2</button>
+        <button type="button" data="3">3</button>
+        <button type="button" data="+" className="operator">+</button>
+      </span>
+      <span className="d-flex row">
+        <button type="button" data="0" className="zero">0</button>
+        <button type="button" data=".">.</button>
+        <button type="button" data="=" className="operator">=</button>
+      </span>
+    </div>
+  );
+};
+
+export default Calculator;
