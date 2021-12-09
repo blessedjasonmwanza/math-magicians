252251af01bf93232584ac66d73bb91bc14a0fb1
farouqdaif1 <daiffarouq@gmail.com>
2021-12-09 14:49:53 +0200

    Add test for calculate

---
+++ src/__tests__/unit/calculate.test.js
@@ -0,0 +1,25 @@
+import calculate from '../../logic/calculate';
+describe('Validate calculate functionality', () => {
+    const num = { total: null, next: null, operation: null };
+    test('multiplication', () => {
+        let result = calculate(num, '10');
+        result = calculate(result, 'x');
+        result = calculate(result, '5');
+        result = calculate(result, '=');
+        expect(result.total).toBe('50');
+    });
+    test('plus', () => {
+        let result = calculate(num, '10');
+        result = calculate(result, '+');
+        result = calculate(result, '5');
+        result = calculate(result, '=');
+        expect(result.total).toBe('15');
+    });
+    test('divide', () => {
+        let result = calculate(num, '10');
+        result = calculate(result, '÷');
+        result = calculate(result, '5');
+        result = calculate(result, '=');
+        expect(result.total).toBe('2');
+    });
+});
