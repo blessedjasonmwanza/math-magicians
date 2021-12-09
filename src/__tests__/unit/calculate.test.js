dc65198c5398f32ec4c22a2fb339816c8d336e33
blessedjasonmwanza <mwanzabj@gmail.com>
2021-12-09 17:18:09 +0200

    Add module by zero and use React Test Library to simulate user interactions

--- src/__tests__/unit/calculate.test.js
+++ src/__tests__/unit/calculate.test.js
@@ -1,4 +1,37 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import calculate from '../../logic/calculate';
+import Calculator from '../../components/calculator';
+
+describe('Calculator test', () => {
+  test('simulate number', () => {
+    render(<Calculator />);
+    const numFour = screen.getByText('4');
+    const output = screen.getByRole('none');
+    fireEvent.click(numFour);
+    expect(output.textContent).toBe('4');
+  });
+  test('simulate operation', () => {
+    render(<Calculator />);
+    const plusSymbol = screen.getByText('+');
+    const output = screen.getByRole('none');
+    fireEvent.click(plusSymbol);
+    expect(output.textContent).toBe('0+');
+  });
+  test('displays correct answer', () => {
+    render(<Calculator />);
+    const two = screen.getByText('2');
+    const three = screen.getByText('9');
+    const plusSymbol = screen.getByText('+');
+    const equal = screen.getByText('=');
+    const output = screen.getByRole('none');
+    fireEvent.click(two);
+    fireEvent.click(plusSymbol);
+    fireEvent.click(three);
+    fireEvent.click(equal);
+    expect(output.textContent).toBe('11');
+  });
+});
 
 describe('Validate calculate functionality', () => {
   const num = { total: null, next: null, operation: null };
@@ -37,5 +70,12 @@ describe('Validate calculate functionality', () => {
     result = calculate(result, '5');
     result = calculate(result, '=');
     expect(result.total).toBe('5');
+  });
+  test('Division by zero', () => {
+    let result = calculate(num, '2');
+    result = calculate(result, '÷');
+    result = calculate(result, '0');
+    result = calculate(result, '=');
+    expect(result.total).toBe('Can\'t divide by 0.');
   });
 });
